be35132f98fa136025ef494a422f1e0995c89ee3
kd2718 <korydonati@gmail.com>
2024-09-11 08:15:12 -0700

    moving things around

--- dev-container/start_app.sh
+++ dev-container/start_app.sh
@@ -4,6 +4,16 @@ set -e
 echo RUNNING MIGRATIONS
 cd /app/db && diesel migration run && cd ..
 
+echo CHECKING DATABASE
+psql --version
+
+TABLE_COUNT=$(psql -h postgres -p 5432 -U hive-dev -d hive-local -c "select count(*) from users" -At)
+if [ $TABLE_COUNT -eq 0 ]; then
+  echo POPULATING DATABASE
+  echo ${psql --version}
+  psql -h postgres -p 5432 -U hive-dev -d hive-local -f /app/dev-container/db_files/hive_dump.sql
+fi
+
 echo STARTING APP
 rustup target add wasm32-unknown-unknown
 cargo leptos watch --hot-reload
